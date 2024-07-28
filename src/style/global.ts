988aeeb72e8aef4ba52cba50877fe63f630d3630
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-07-28 17:50:18 +0900

    Feat: Add theme.test.ts, Font 적용

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 400;
+        src: url('https://cdn.jsdelivr.net/npm/pretendard@1.3.5/dist/web/static/pretendard.css');
+    }
     body {
         padding : 0;
         width: 100%;
         max-width: 600px;
         margin: 0 auto;
         background-color: ${({ theme }) => theme.color.background};
+        font-family: 'Pretendard', sans-serif;
     }
 `;
