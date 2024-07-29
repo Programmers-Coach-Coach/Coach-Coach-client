d4524b714dd1877a3cc5d80e32e6e881f4ff6f8d
하주영 <ju980304@naver.com>
2024-07-29 23:46:09 +0900

    Feat: 모달 데이터 저장하는 파일 생성

--- src/data/modal.ts
+++ src/data/modal.ts
@@ -1,3 +1,5 @@
+import { ICONS } from "../constants/assets"; // 수정된 import 경로
+
 export type TModal =
   | "star-enroll"
   | "routine-enroll"
@@ -8,7 +10,25 @@ export type TModal =
   | "sports-enroll"
   | "sports-modify";
 
-export const modal: Record<TModal, { button: string }> = {
+export type TFooterPicker = "profile" | "routine";
+export type TRoutinePicker = "write";
+
+export interface IModalData {
+  button: string;
+}
+
+export interface IFooterPickerData {
+  name: string;
+  link: string;
+  icon: JSX.Element;
+}
+
+export interface IRoutinePickerData {
+  name: string;
+  onClick: () => void;
+}
+
+export const modal: Record<TModal, IModalData> = {
   "star-enroll": {
     button: "등록하기"
   },
@@ -35,20 +55,30 @@ export const modal: Record<TModal, { button: string }> = {
   }
 };
 
-export type TFooterPicker = "profile" | "routine";
-
-export const footerPicker: Record<
-  TFooterPicker,
-  { name: string; link: string }[]
-> = {
+export const footerPicker: Record<TFooterPicker, IFooterPickerData[]> = {
   profile: [
-    { name: "내 프로필", link: "" },
-    { name: "매칭 회원 리스트", link: "" },
-    { name: "내기록", link: "" },
-    { name: "로그아웃", link: "" }
+    { name: "내 프로필", link: "/", icon: ICONS.human.one },
+    {
+      name: "매칭 회원 리스트",
+      link: "",
+      icon: ICONS.human.two
+    },
+    { name: "내 기록", link: "/", icon: ICONS.record },
+    { name: "로그아웃", link: "/", icon: ICONS.logout }
   ],
   routine: [
-    { name: "내 루틴", link: "" },
-    { name: "내 코치 루틴", link: "" }
+    { name: "내 루틴", link: "/", icon: ICONS.human.one },
+    {
+      name: "내 코치 루틴",
+      link: "/",
+      icon: ICONS.human.coach
+    }
+  ]
+};
+
+export const routinePicker: Record<TRoutinePicker, IRoutinePickerData[]> = {
+  write: [
+    { name: "루틴 작성", onClick: () => {} },
+    { name: "삭제", onClick: () => {} }
   ]
 };
