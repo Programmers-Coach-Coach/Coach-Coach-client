73b26a471c4a565111d36eb0067a59df6ef249ab
하주영 <ju980304@naver.com>
2024-07-29 15:15:31 +0900

    Refactor: 폴더 구조 추가 및 빈 파일 생성

---
+++ src/components/common/Title.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Title = () => {
+  return (
+    <TitleStyle>
+      <h1>Title body</h1>
+    </TitleStyle>
+  );
+};
+
+const TitleStyle = styled.div``;
+export default Title;
