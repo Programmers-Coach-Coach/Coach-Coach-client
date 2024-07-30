545919792ffd682b78f9bf82300fd11c34ed2af5
yoonsaeng <truestar1997@gmail.com>
2024-07-30 18:04:16 +0900

    Add: react-icons 추가

--- src/constants/assets.tsx
+++ src/constants/assets.tsx
@@ -3,13 +3,23 @@ import { IoMdPerson } from "react-icons/io";
 import { FaPeoplePulling } from "react-icons/fa6";
 import { IoLogOutOutline } from "react-icons/io5";
 import { PiSunglassesBold } from "react-icons/pi";
+import { IoIosArrowBack } from "react-icons/io";
+import { GrHomeRounded } from "react-icons/gr";
+import { CiDumbbell } from "react-icons/ci";
+import { CiBellOn } from "react-icons/ci";
+import { RxPerson } from "react-icons/rx";
 
 export const ICONS = {
   human: {
     one: <IoMdPerson />,
     two: <FaPeoplePulling />,
-    coach: <PiSunglassesBold />
+    coach: <PiSunglassesBold />,
+    profile: <RxPerson />
   },
   record: <HiChartBar />,
-  logout: <IoLogOutOutline />
+  logout: <IoLogOutOutline />,
+  back: <IoIosArrowBack />,
+  home: <GrHomeRounded />,
+  routine: <CiDumbbell />,
+  alarm: <CiBellOn />
 };
