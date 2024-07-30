0b8786959c53a747bd31ede11e28da75f24a5bff
하주영 <ju980304@naver.com>
2024-07-30 18:54:51 +0900

    Design: title, body 폰트 설정

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -15,6 +15,10 @@ export type ButtonSize = "large" | "small";
 export type ProfileImageSize = "small" | "medium" | "large";
 export type ModalSize = "default";
 export type PaddingSize = "default";
+
+export type TitleSize = "t1" | "t2";
+export type BodySize = "b1" | "b2" | "b3";
+
 interface Theme {
   color: Record<ColorKey, string>;
   fontSize: Record<FontSize, string>;
@@ -43,6 +47,20 @@ interface Theme {
   };
   padding: {
     [key in PaddingSize]: string;
+  };
+  titleSize: {
+    [key in TitleSize]: {
+      fontSize: string;
+      lineHeight: string;
+      bold: string;
+    };
+  };
+  bodySize: {
+    [key in BodySize]: {
+      fontSize: string;
+      lineHeight: string;
+      bold: string;
+    };
   };
 }
 
@@ -103,5 +121,34 @@ export const theme: Theme = {
   },
   padding: {
     default: "50px"
+  },
+  titleSize: {
+    t1: {
+      fontSize: "20px",
+      lineHeight: "36px",
+      bold: "800"
+    },
+    t2: {
+      fontSize: "16px",
+      lineHeight: "24px",
+      bold: "800"
+    }
+  },
+  bodySize: {
+    b1: {
+      fontSize: "16px",
+      lineHeight: "16px",
+      bold: "400"
+    },
+    b2: {
+      fontSize: "12px",
+      lineHeight: "16px",
+      bold: "400"
+    },
+    b3: {
+      fontSize: "16px",
+      lineHeight: "32px",
+      bold: "800"
+    }
   }
 };
