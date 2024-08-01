e02fcd717e4b0d469a10866a1e9f648dc37a5edd
yoonsaeng <truestar1997@gmail.com>
2024-08-02 02:23:22 +0900

    Refactor: 이중 객체 제거

--- src/constants/assets.tsx
+++ src/constants/assets.tsx
@@ -1,25 +1,21 @@
 import { HiChartBar } from "react-icons/hi2";
-import { IoMdPerson } from "react-icons/io";
+import { IoMdPerson, IoIosArrowBack } from "react-icons/io";
 import { FaPeoplePulling } from "react-icons/fa6";
 import { IoLogOutOutline } from "react-icons/io5";
 import { PiSunglassesBold } from "react-icons/pi";
-import { IoIosArrowBack } from "react-icons/io";
 import { GrHomeRounded } from "react-icons/gr";
-import { CiDumbbell } from "react-icons/ci";
-import { CiBellOn } from "react-icons/ci";
+import { CiDumbbell, CiBellOn } from "react-icons/ci";
 import { RxPerson } from "react-icons/rx";
 
 export const ICONS = {
-  human: {
-    one: <IoMdPerson />,
-    two: <FaPeoplePulling />,
-    coach: <PiSunglassesBold />,
-    profile: <RxPerson />
-  },
-  record: <HiChartBar />,
-  logout: <IoLogOutOutline />,
-  back: <IoIosArrowBack />,
-  home: <GrHomeRounded />,
-  routine: <CiDumbbell />,
-  alarm: <CiBellOn />
+  person: IoMdPerson,
+  twins: FaPeoplePulling,
+  coach: PiSunglassesBold,
+  profile: RxPerson,
+  record: HiChartBar,
+  logout: IoLogOutOutline,
+  back: IoIosArrowBack,
+  home: GrHomeRounded,
+  routine: CiDumbbell,
+  alarm: CiBellOn
 };
