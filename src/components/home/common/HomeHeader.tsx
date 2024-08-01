01f6632eaee26af0089d9f5b1d867eacebc1d65c
하주영 <ju980304@naver.com>
2024-08-01 18:40:56 +0900

    Feat: 홈 화면에서 사용하는 title, subtitle 구현

--- src/components/home/common/HomeHeader.tsx
+++ src/components/home/common/HomeHeader.tsx
@@ -18,13 +18,14 @@ const HomeHeader = ({ title, subTitle, showAll = false }: Props) => {
 const HomeHeaderStyle = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 8px;
   position: relative;
   width: 100%;
   color: ${({ theme }) => theme.color.white};
 
   .showall {
     position: absolute;
-    top: 0;
+    top: 4px;
     right: 0;
     color: #5b818e;
   }
