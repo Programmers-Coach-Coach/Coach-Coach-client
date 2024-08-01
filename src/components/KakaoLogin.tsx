fd66457a0591ab4cb0985d3522a3ed4ab82aa359
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-01 19:14:17 +0900

    Fix: KakaoLogin.tsx 주석제거

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -1,11 +1,9 @@
-// KakaoLogin.tsx
 import React from "react";
 import { Button, ButtonProps } from "@mui/material";
 import { styled } from "@mui/system";
 import image from "../assets/images/kakao-login.png";
 import { theme } from "../style/theme";
 
-// Styled Button 컴포넌트
 const KakaoButton = styled(Button)<ButtonProps>(() => ({
   width: "314px",
   height: "48px",
