e53d3cc82ae4e0a520e3a81daf07fc02427ffa73
하주영 <ju980304@naver.com>
2024-08-02 22:28:02 +0900

    Interface: 메인 api 인터페이스 정의

---
+++ src/models/coach.model.ts
@@ -0,0 +1,12 @@
+export interface IPopularCoach {
+  coachId: number;
+  coachName: string;
+  coachImageUrl: string;
+  description: string;
+  countOfLikes: number;
+  liked: boolean;
+  coachingSports: {
+    sportId: number;
+    sportName: string;
+  }[];
+}
