f23d6ad6bd097b00a82c10d412d1b4e052444532
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-03 22:31:22 +0900

    Feat: Signup 유효성 검사 추가

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,8 +1,10 @@
+import { useForm, Controller } from "react-hook-form";
 import styled from "styled-components";
 import Logo from "../assets/images/Logo.png";
 import AuthInput from "@/components/common/InputField/Text/AuthInput";
 import CustomButton from "@/components/common/Button/CustomButton";
 import { Link } from "react-router-dom";
+import { SignupProps } from "@/models/auth.model";
 
 const Container = styled.div`
   width: 100%;
@@ -15,14 +17,11 @@ const Container = styled.div`
   padding: 0 20px;
   margin: 0 auto;
   box-sizing: border-box;
-  gap: 20px;
+  gap: 10px;
 `;
 
 const ImageWrapper = styled.img`
   width: 50%;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
 `;
 
 const LinkWrapper = styled.div`
@@ -45,60 +44,150 @@ const FlexContainer = styled.div`
   gap: 20px;
 `;
 
+const ErrorText = styled.div`
+  color: red;
+  text-align: left;
+  width: 314px;
+`;
+
+const NICKNAME_REGEX =
+  /^(?!.*\s{2,})([0-9a-zA-Z가-힣][0-9a-zA-Z가-힣\s]{0,8}[0-9a-zA-Z가-힣])$/;
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const PASSWORD_REGEX =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/;
+
 function Signup() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    getValues
+  } = useForm<SignupProps>({ mode: "onBlur" });
+
+  const onSubmit = (data: SignupProps) => {
+    const signupData = {
+      nickname: data.nickname,
+      email: data.email,
+      password: data.password
+    };
+
+    userSignup(signupData);
+  };
+
   return (
-    <>
-      <Container>
-        <ImageWrapper className="logo" src={Logo} />
-        <FlexContainer>
-          <AuthInput
-            label="닉네임"
-            name="nickname"
-            type="text"
-            width="212px"
-            placeholder="닉네임을 입력하세요"
-          />
-          <CustomButton size="small" variant="outlined">
-            <span className="b2">중복확인</span>
-          </CustomButton>
-        </FlexContainer>
-        <FlexContainer>
-          <AuthInput
-            label="이메일"
-            name="email"
-            type="email"
-            width="212px"
-            placeholder="example@naver.com"
-          />
-          <CustomButton size="small" variant="outlined">
-            <span className="b2">중복확인</span>
-          </CustomButton>
-        </FlexContainer>
-        <AuthInput
-          label="비밀번호"
-          name="password"
-          type="password"
-          placeholder="비밀번호"
+    <Container>
+      <ImageWrapper className="logo" src={Logo} />
+      <FlexContainer>
+        <Controller
+          name="nickname"
+          control={control}
+          rules={{
+            required: "닉네임을 입력하세요",
+            pattern: {
+              value: NICKNAME_REGEX,
+              message: "닉네임은 공백제외 1~10자입니다."
+            }
+          }}
+          render={({ field }) => (
+            <AuthInput
+              {...field}
+              label="닉네임"
+              type="text"
+              width="212px"
+              placeholder="닉네임을 입력하세요"
+            />
+          )}
         />
-        <AuthInput
-          label="비밀번호 확인"
-          name="passwordCheck"
-          type="password"
-          placeholder="비밀번호 확인"
-        />
-
-        <CustomButton size="large" variant="contained">
-          가입하기
+        <CustomButton size="small" variant="outlined">
+          <span className="b2">중복확인</span>
         </CustomButton>
+      </FlexContainer>
+      <ErrorText className="b2">{errors.nickname?.message ?? ""}</ErrorText>
 
-        <LinkWrapper>
-          계정이 있으신가요?{" "}
-          <Link to="/auth/signup">
-            <span className="login">로그인 하러가기</span>
-          </Link>
-        </LinkWrapper>
-      </Container>
-    </>
+      <FlexContainer>
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: "이메일을 입력하세요",
+            pattern: {
+              value: EMAIL_REGEX,
+              message: "올바른 이메일 주소를 입력하세요."
+            }
+          }}
+          render={({ field }) => (
+            <AuthInput
+              {...field}
+              label="이메일"
+              type="email"
+              width="212px"
+              placeholder="example@naver.com"
+            />
+          )}
+        />
+        <CustomButton size="small" variant="outlined">
+          <span className="b2">중복확인</span>
+        </CustomButton>
+      </FlexContainer>
+      <ErrorText className="b2">{errors.email?.message ?? ""}</ErrorText>
+
+      <Controller
+        name="password"
+        control={control}
+        rules={{
+          required: "비밀번호를 입력하세요",
+          pattern: {
+            value: PASSWORD_REGEX,
+            message: "비밀번호는 숫자, 특수문자 포함 8~20자입니다."
+          }
+        }}
+        render={({ field }) => (
+          <AuthInput
+            {...field}
+            label="비밀번호"
+            type="password"
+            placeholder="비밀번호"
+          />
+        )}
+      />
+      <ErrorText className="b2">{errors.password?.message ?? ""}</ErrorText>
+
+      <Controller
+        name="passwordCheck"
+        control={control}
+        rules={{
+          required: "비밀번호 확인을 입력하세요",
+          validate: (value: string) =>
+            value === getValues("password") || "비밀번호가 일치하지 않습니다."
+        }}
+        render={({ field }) => (
+          <AuthInput
+            {...field}
+            label="비밀번호 확인"
+            type="password"
+            placeholder="비밀번호 확인"
+          />
+        )}
+      />
+      <ErrorText className="b2">
+        {errors.passwordCheck?.message ?? ""}
+      </ErrorText>
+
+      <CustomButton
+        size="large"
+        variant="contained"
+        onClick={handleSubmit(onSubmit)}
+      >
+        가입하기
+      </CustomButton>
+
+      <LinkWrapper>
+        계정이 있으신가요?{" "}
+        <Link to="/auth/login">
+          <span className="login">로그인 하러가기</span>
+        </Link>
+      </LinkWrapper>
+    </Container>
   );
 }
 
