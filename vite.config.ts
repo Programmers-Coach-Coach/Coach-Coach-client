42bfca6e337605b7367bb95cd9e6e542713c3368
하주영 <92720304+hazzuu123@users.noreply.github.com>
2024-08-05 18:49:04 +0900

    Remove: vite proxy 삭제 (#78)

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/s
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        ws: true
-      }
-    }
-  }
+  plugins: [react(), tsconfigPaths()]
 });
