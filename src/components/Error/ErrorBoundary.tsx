77b78ff7dc7499803f8602b5d3f7910ccddcf054
yoonsaeng <truestar1997@gmail.com>
2024-08-06 13:42:57 +0900

    Feat: ErrorBoundary 추가

---
+++ src/components/Error/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import React, { Component, ReactNode } from "react";
+import * as Sentry from "@sentry/react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    Sentry.setExtra("errorInfo", errorInfo);
+    Sentry.captureException(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
