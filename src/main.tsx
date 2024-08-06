5adbc59f2197ce48add5ffcd29e83cb198ef8fea
yoonsaeng <truestar1997@gmail.com>
2024-08-06 13:44:13 +0900

    Add: Sentry 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,21 @@ import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./api/queryClient.ts";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", import.meta.env.VITE_SENTRY_URL],
+  profilesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
