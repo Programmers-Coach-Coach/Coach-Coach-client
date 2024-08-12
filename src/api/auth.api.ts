b80e7cd9036dbe24a62672c460c8e297fdf60fd2
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-12 17:21:11 +0900

    Feat: 로그인 auth.api.ts 작성

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,3 +1,4 @@
+import { ILogin } from "@/models/auth.model";
 import { requestHandler } from "./http";
 import { API_PATH } from "@/constants/apiPath";
 
@@ -7,4 +8,8 @@ export const getProfile = () => {
 
 export const getCoachProfile = () => {
   return requestHandler("get", API_PATH.coachMypage);
+};
+
+export const login = (formData: ILogin) => {
+  return requestHandler("post", "/auth/login", formData);
 };
