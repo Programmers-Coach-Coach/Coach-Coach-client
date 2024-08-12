c09db32231eba81e77170113728b4e8a8aab8a40
하주영 <ju980304@naver.com>
2024-08-12 19:42:39 +0900

    Design: 필터 아이콘 추가

--- src/constants/assets.tsx
+++ src/constants/assets.tsx
@@ -1,11 +1,12 @@
-import { HiChartBar } from "react-icons/hi2";
-import { IoMdPerson, IoIosArrowBack } from "react-icons/io";
+import { CiBellOn, CiDumbbell } from "react-icons/ci";
 import { FaPeoplePulling } from "react-icons/fa6";
+import { GrHomeRounded } from "react-icons/gr";
+import { HiChartBar } from "react-icons/hi2";
+import { IoIosArrowBack, IoMdPerson } from "react-icons/io";
 import { IoLogOutOutline } from "react-icons/io5";
 import { PiSunglassesBold } from "react-icons/pi";
-import { GrHomeRounded } from "react-icons/gr";
-import { CiDumbbell, CiBellOn } from "react-icons/ci";
 import { RxPerson } from "react-icons/rx";
+import { TbArrowsExchange2 } from "react-icons/tb";
 
 export const ICONS = {
   person: IoMdPerson,
@@ -17,5 +18,6 @@ export const ICONS = {
   back: IoIosArrowBack,
   home: GrHomeRounded,
   routine: CiDumbbell,
-  alarm: CiBellOn
+  alarm: CiBellOn,
+  filter: TbArrowsExchange2
 };
