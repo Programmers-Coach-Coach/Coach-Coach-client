deedf6eae193b8263fb38fcdb6a683209ea42fbb
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-12 17:23:24 +0900

    Fix: Login.tsx에 react-hook-form 적용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,61 @@
 import styled from "styled-components";
 import Logo from "../assets/images/Logo.png";
-import AuthInput from "@/components/common/InputField/Text/AuthInput";
 import CustomButton from "@/components/common/Button/CustomButton";
 import { Link } from "react-router-dom";
 import KakaoLogin from "@/components/common/Button/KakaoLogin";
+import { useForm, Controller } from "react-hook-form";
+import { ILogin } from "@/models/auth.model";
+import useAuth from "@/hooks/useAuth";
+import AuthInput from "@/components/common/InputField/Text/AuthInput";
 
 function Login() {
+  const { control, handleSubmit } = useForm<ILogin>();
+  const { userLogin } = useAuth();
+  const onSubmit = (data: ILogin) => {
+    userLogin(data);
+  };
   return (
     <>
       <Container>
         <ImageWrapper className="logo" src={Logo} />
+        <FormWrapper onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <AuthInput
+                {...field}
+                label="이메일"
+                name="email"
+                type="email"
+                placeholder="example@naver.com"
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <AuthInput
+                {...field}
+                type="password"
+                label="비밀번호"
+                name="password"
+                placeholder="비밀번호"
+              />
+            )}
+          />
 
-        <AuthInput
-          label="이메일"
-          name="email"
-          type="email"
-          placeholder="example@naver.com"
-        />
-        <AuthInput
-          label="비밀번호"
-          name="password"
-          type="password"
-          placeholder="비밀번호"
-        />
+          <CustomButton
+            size="large"
+            variant="contained"
+            onClick={handleSubmit(onSubmit)}
+            type="submit"
+          >
+            로그인
+          </CustomButton>
+        </FormWrapper>
 
-        <CustomButton size="large" variant="contained">
-          로그인
-        </CustomButton>
         <KakaoLogin></KakaoLogin>
         <LinkWrapper className="b1">
           계정이 없으신가요?{" "}
@@ -53,6 +82,11 @@ const Container = styled.div`
   gap: 15px;
 `;
 
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
 const ImageWrapper = styled.img`
   width: 314px;
   text-align: center;
