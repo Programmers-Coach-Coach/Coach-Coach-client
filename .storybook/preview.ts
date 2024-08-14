09a61ab77eb7ab50f67cdcf2927595c2b9190995
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-14 21:30:47 +0900

    Feat: 자주 쓰이는 컴포넌트에 대한 storybook 적용

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,8 @@
 import type { Preview } from "@storybook/react";
+import previewDecorators from "./preview-decorators";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i
-      }
-    }
-  }
+  decorators: previewDecorators
 };
 
 export default preview;
