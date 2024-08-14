1a01b8ac44c705f1f58f16b92153eb83f0effa68
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-14 23:57:33 +0900

    비밀번호 확인을 했는지 알 수 있는 상태 추가

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -3,18 +3,32 @@ import { devtools } from "zustand/middleware";
 
 interface StoreState {
   isLoggedIn: boolean;
+  isPasswordConfirmed: boolean;
   storeLogin: () => void;
   storeLogout: () => void;
+  confirmPassword: () => void;
+  resetPasswordConfirmation: () => void;
 }
 
 export const useAuthStore = create<StoreState>()(
   devtools((set) => ({
     isLoggedIn: false,
+    isPasswordConfirmed: false,
+
     storeLogin: () => {
       set({ isLoggedIn: true });
     },
+
     storeLogout: () => {
-      set({ isLoggedIn: false });
+      set({ isLoggedIn: false, isPasswordConfirmed: false });
+    },
+
+    confirmPassword: () => {
+      set({ isPasswordConfirmed: true });
+    },
+
+    resetPasswordConfirmation: () => {
+      set({ isPasswordConfirmed: false });
     }
   }))
 );
