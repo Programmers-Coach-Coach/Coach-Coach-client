3f6111e1876b25b2143a06850d31b7148af91d06
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-16 20:47:46 +0900

    Feat: 로딩 공통 컴포넌트 구현

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,22 @@
+import { FadeLoader } from "react-spinners";
+import styled from "styled-components";
+
+const Loading = () => {
+  return (
+    <Container>
+      <h3>잠시만 기다려주세요.</h3>
+      <FadeLoader />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  height: 70vh;
+`;
+
+export default Loading;
