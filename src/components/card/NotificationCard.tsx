c9e7c5d8c08534a13ef20a67d251128e4d9d79a0
하주영 <ju980304@naver.com>
2024-08-16 20:41:33 +0900

    Feat: 알림 페이지 UI 구현

---
+++ src/components/card/NotificationCard.tsx
@@ -0,0 +1,81 @@
+import Heart from "@/assets/images/notice-heart.png";
+import Message from "@/assets/images/notice-message.png";
+import Speaker from "@/assets/images/notice-speaker.png";
+
+import { TNotificationType } from "@/models/notification.model";
+import styled from "styled-components";
+import Icon from "../Icon/Icon";
+
+interface Props {
+  noticeId: number;
+  relationFunction: TNotificationType;
+  message: string;
+  createdAt: string;
+}
+
+const NotificationCard = ({
+  noticeId,
+  relationFunction,
+  message,
+  createdAt
+}: Props) => {
+  const getImageSrc = () => {
+    switch (relationFunction) {
+      case "review":
+        return Message;
+      case "ask":
+        return Speaker;
+      default:
+        return Heart;
+    }
+  };
+
+  const handleDelete = (id: number) => {
+    // TODO: 알림 개별 삭제
+  };
+
+  return (
+    <ReviewCardStyle>
+      <img src={getImageSrc()} alt="" />
+      <div className="message">{message}</div>
+      <button
+        onClick={() => {
+          handleDelete(noticeId);
+        }}
+        className="close-button"
+      >
+        <Icon name="x" size="18px" color="gray3" />
+      </button>
+      <div className="timestamp">{createdAt}</div>
+    </ReviewCardStyle>
+  );
+};
+
+const ReviewCardStyle = styled.div`
+  display: flex;
+  align-items: center;
+
+  gap: 14px;
+  min-height: 90px;
+  padding: 0 10px;
+  position: relative;
+  border-radius: ${({ theme }) => theme.borderRadius.default};
+  box-shadow: ${({ theme }) => theme.boxShadow};
+
+  .message {
+    flex: 1;
+    word-break: keep-all;
+  }
+  .close-button {
+    margin-left: auto;
+  }
+  .timestamp {
+    position: absolute;
+    bottom: 12px;
+    right: 40px;
+    font-size: 12px;
+    color: ${({ theme }) => theme.color.gray3};
+  }
+`;
+
+export default NotificationCard;
--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,12 +1,44 @@
+import NotificationCard from "@/components/card/NotificationCard";
+import { notification } from "@/data/notification";
 import styled from "styled-components";
 
 const Notification = () => {
+  const handleDelete = () => {
+    // TODO: 알림 전체 삭제
+  };
   return (
     <NotificationStyle>
-      <h1>Notification body</h1>
+      <Button onClick={handleDelete}>전체 삭제</Button>
+      <div className="contents">
+        {notification.map((item) => (
+          <NotificationCard
+            noticeId={item.noticeId}
+            message={item.message}
+            relationFunction={item.relationFunction}
+            createdAt={item.createdAt}
+          />
+        ))}
+      </div>
     </NotificationStyle>
   );
 };
 
-const NotificationStyle = styled.div``;
+const NotificationStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  padding-bottom: 80px;
+
+  .contents {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+  }
+`;
+
+const Button = styled.button`
+  color: ${({ theme }) => theme.color.gray3};
+  display: flex;
+  margin-left: auto;
+`;
 export default Notification;
