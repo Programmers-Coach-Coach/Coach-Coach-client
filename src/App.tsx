9a5ae522b7e80926cac2ba1f7e0970b3b0c36ac6
하주영 <ju980304@naver.com>
2024-08-17 03:04:41 +0900

    Feat: 기록 상세 페이지 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import styled, { ThemeProvider } from "styled-components";
-import { theme } from "./style/theme";
-import { GlobalStyle } from "./style/global";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+import { queryClient } from "./api/queryClient";
 import Layout from "./components/layout/Layout";
+import CheckPassword from "./pages/CheckPassword";
+import CoachList from "./pages/CoachList";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./api/queryClient";
 import Mypage from "./pages/Mypage";
-import { Toaster } from "react-hot-toast";
-import CoachList from "./pages/CoachList";
 import MyRoutine from "./pages/MyRoutine";
+import RecordDatail from "./pages/RecordDatail";
 import Signup from "./pages/Signup";
-import CheckPassword from "./pages/CheckPassword";
+import { GlobalStyle } from "./style/global";
+import { theme } from "./style/theme";
 
 const router = createBrowserRouter([
   {
@@ -46,6 +47,10 @@ const router = createBrowserRouter([
       {
         path: "check-password",
         element: <CheckPassword />
+      },
+      {
+        path: "record/:id",
+        element: <RecordDatail />
       }
     ]
   }
--- src/utils/getTitle.ts
+++ src/utils/getTitle.ts
@@ -10,6 +10,8 @@ export const getTitle = (pathname: string): string => {
       return "루틴";
     case "/check-password":
       return "본인확인";
+    case "/record/:id":
+      return "내 기록";
     default:
       return "";
   }
