017936a5c8a7b6a0d39836285ec59c6196d9861f
하주영 <ju980304@naver.com>
2024-08-16 19:19:44 +0900

    Feat: 알림 페이지 연결

---
+++ src/pages/Notification.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Notification = () => {
+  return (
+    <NotificationStyle>
+      <h1>Notification body</h1>
+    </NotificationStyle>
+  );
+};
+
+const NotificationStyle = styled.div``;
+export default Notification;
