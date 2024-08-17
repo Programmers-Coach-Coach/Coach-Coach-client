2b3ef241d389b545ff1d935ab5a376cdb5a76bfe
yoonsaeng <truestar1997@gmail.com>
2024-08-18 07:50:20 +0900

    Refactor: Footer z 좌표 값 수정

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -79,5 +79,6 @@ const FooterStyle = styled.footer`
   width: 100%;
   max-width: 600px;
   height: 60px;
+  z-index: 1001; // FooterAbove(Footer 아이콘 클릭 시 모달)보다 앞에 있어 FooterAbove가 가리지 않게 함
 `;
 export default Footer;
