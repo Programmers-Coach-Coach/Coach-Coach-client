576330be86c97e7ed9962fc0cda1796a139f85da
하주영 <ju980304@naver.com>
2024-08-18 23:48:41 +0900

    Feat: 기록 페이지 url 수정

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
         element: <CheckPassword />
       },
       {
-        path: "record/:id",
+        path: "record",
         element: <RecordDatail />
       }
     ]
