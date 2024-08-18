96fb205961b8fecd03dc6d94f9f13f2a50a4bf23
yoonsaeng <truestar1997@gmail.com>
2024-08-18 09:04:53 +0900

    Feat: react-snap(hydrate) prerendering setting

--- src/main.tsx
+++ src/main.tsx
@@ -27,9 +27,13 @@ async function enableMocking() {
 }
 
 enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  const rootElement = document.getElementById("root");
+  if (rootElement) {
+    ReactDOM.hydrateRoot(
+      rootElement,
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
 });
