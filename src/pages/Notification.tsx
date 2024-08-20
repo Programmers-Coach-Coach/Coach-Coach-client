4b510a7acfa0f93871429fa7d9bf6116021797ae
하주영 <ju980304@naver.com>
2024-08-21 01:48:33 +0900

    Feat: 알림 전체 조회 API 연결

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,24 +1,42 @@
 import NotificationCard from "@/components/card/NotificationCard";
-import { notification } from "@/data/notification";
+import Empty from "@/components/common/Empty/Empty";
+import Loading from "@/components/loading/Loading";
+import { useFetchNotifications } from "@/hooks/queries/useNotification";
 import styled from "styled-components";
 
 const Notification = () => {
   const handleDelete = () => {
     // TODO: 알림 전체 삭제
   };
+
+  const { data, isError, isLoading } = useFetchNotifications();
+
+  if (isLoading) return <Loading />;
+  if (isError || !data)
+    return <div>알림 정보를 가져오는 중 오류가 발생했습니다.</div>;
+
   return (
     <NotificationStyle>
       <Button onClick={handleDelete}>전체 삭제</Button>
       <div className="contents">
-        {notification.map((item) => (
-          <NotificationCard
-            key={item.noticeId}
-            noticeId={item.noticeId}
-            message={item.message}
-            relationFunction={item.relationFunction}
-            createdAt={item.createdAt}
+        {data.length ? (
+          data.map((item) => (
+            <NotificationCard
+              key={item.noticeId}
+              noticeId={item.noticeId}
+              message={item.message}
+              relationFunction={item.relationFunction}
+              createdAt={item.createdAt}
+            />
+          ))
+        ) : (
+          <Empty
+            name="alarm"
+            size="150px"
+            color="text"
+            descriptions="모든 알림을 확인하였습니다."
           />
-        ))}
+        )}
       </div>
     </NotificationStyle>
   );
@@ -42,4 +60,5 @@ const Button = styled.button`
   display: flex;
   margin-left: auto;
 `;
+
 export default Notification;
