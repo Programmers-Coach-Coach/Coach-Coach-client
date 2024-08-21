c81ebb27292107225d502d7fd03dbcaff99051b5
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2024-08-21 19:58:10 +0900

    Update index.ts

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,11 +1,9 @@
 import { handlers as coachHandlers } from "./coach";
 import { handlers as routineHandlers } from "./routine";
 import { handlers as memberHandler } from "./member";
-import { handlers as profileHandlers } from "./profile";
 
 export const handlers = [
   ...coachHandlers,
-  ...profileHandlers,
   ...routineHandlers,
   ...memberHandler
 ];
