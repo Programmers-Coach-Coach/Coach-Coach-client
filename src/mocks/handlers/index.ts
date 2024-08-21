ccc82c0c8eb4d1efaf02b12b4029067fa7acc7b8
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-08-21 15:35:47 +0900

    Fix: mock 핸들러 수정

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,11 +1,4 @@
 import { handlers as coachHandlers } from "./coach";
-import { handlers as homeHandlers } from "./home";
 import { handlers as routineHandlers } from "./routine";
 
-export const handlers = [...homeHandlers, ...coachHandlers, ...routineHandlers];
-import { handlers as routineHandlers } from "./routine";
-
-export const handlers = [
-  ...coachHandlers,
-  ...routineHandlers
-];
+export const handlers = [...coachHandlers, ...routineHandlers];
