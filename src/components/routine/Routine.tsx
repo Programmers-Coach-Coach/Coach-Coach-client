1c46abfa5f9f1f579092e19353cd974d4c9f7d2f
yoonsaeng <truestar1997@gmail.com>
2024-08-23 05:44:43 +0900

    Add: 운동 상세 페이지 경로 변경 코드 추가

--- src/components/routine/Routine.tsx
+++ src/components/routine/Routine.tsx
@@ -9,7 +9,7 @@ import RoutineContents from "@/components/common/modal/contents/RoutineContents"
 import { SPORTS_NAMES } from "@/constants/sportsConstants";
 import { useModalInfo } from "@/store/modalInfo.store";
 import RoutinePicker from "../common/modal/contents/RoutinePicker";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface RoutineProps {
   id: number;
@@ -19,6 +19,7 @@ interface RoutineProps {
 
 const Routine = ({ id, name, sportId }: RoutineProps) => {
   const location = useLocation();
+  const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
 
   const modifyModal = useModal();
@@ -58,7 +59,12 @@ const Routine = ({ id, name, sportId }: RoutineProps) => {
         </Modal>
       )}
       <Card>
-        <RoutineStyle>
+        <RoutineStyle
+          onClick={() => {
+            setRoutineId(id);
+            navigate(`/routine/detail/${id}?routineName=${name}`);
+          }}
+        >
           <RoutineTextStyle>
             <h2>{name}</h2>
             <p className="b2">{SPORTS_NAMES[sportId]}</p>
