fe971b3597ea9f77b65aeaece3063237c14075ab
하주영 <ju980304@naver.com>
2024-08-22 22:46:20 +0900

    Feat: 임시

--- src/components/common/Header/LogoHeader.tsx
+++ src/components/common/Header/LogoHeader.tsx
@@ -1,21 +1,44 @@
+import { useFetchAuth } from "@/hooks/useFetchAuth";
 import { styled } from "styled-components";
 import Logo from "../../../assets/images/Logo.png";
 
-const LogoHeader = () => {
+interface Props {
+  nickname?: string;
+}
+const LogoHeader = ({ nickname }: Props) => {
+  const { data } = useFetchAuth();
   return (
     <LogoHeaderStyle>
       <img src={Logo} alt="Logo" />
+      {data?.nickname && (
+        <h2>
+          <span>안녕하세요! </span>
+          {data?.nickname}
+          <span>님</span>
+        </h2>
+      )}
     </LogoHeaderStyle>
   );
 };
 
 const LogoHeaderStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 20px;
   margin: 0;
 
   img {
     width: 90px;
     height: 50px;
+  }
+
+  h2 {
+    display: flex;
+    gap: 6px;
+  }
+  span {
+    font-size: 12px;
   }
 `;
 
