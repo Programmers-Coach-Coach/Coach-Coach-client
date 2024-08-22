58345d1f3b574539a0e027599312e1dde7a8212c
yoonsaeng <truestar1997@gmail.com>
2024-08-23 05:43:45 +0900

    Feat: MSW 운동 api

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,9 +1,13 @@
 import { handlers as coachHandlers } from "./coach";
-import { handlers as routineHandlers } from "./routine";
+import { handlers as routineHandlers } from "./routine/routine";
 import { handlers as memberHandler } from "./member";
+import { handlers as categoryHandler } from "./routine/category";
+import { handlers as actionHandler } from "./routine/action";
 
 export const handlers = [
   ...coachHandlers,
   ...routineHandlers,
-  ...memberHandler
+  ...memberHandler,
+  ...categoryHandler,
+  ...actionHandler
 ];
