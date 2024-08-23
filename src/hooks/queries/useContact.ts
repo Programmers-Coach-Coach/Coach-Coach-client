54ca9a877e1b8cdddcd09a43bcb03e8317f7a254
하주영 <ju980304@naver.com>
2024-08-23 13:51:45 +0900

    Feat: 이미 문의했던 코치에 대해서 토스트 메세지 띄움

--- src/hooks/queries/useContact.ts
+++ src/hooks/queries/useContact.ts
@@ -1,11 +1,18 @@
 import { contact } from "@/api/coach.api";
 import { IResponseMessage } from "@/models/responseMessage.model";
+import { conflictError } from "@/utils/error";
 import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 export const useContact = () => {
   const { mutate, isError } = useMutation<IResponseMessage, Error, number>({
     mutationFn: (id: number) => contact(id),
-    onSuccess: () => {}
+    onSuccess: () => {},
+    onError: (error) => {
+      if (conflictError(error)) {
+        toast.error("이미 문의가 완료된 코치입니다");
+      }
+    }
   });
 
   return {
