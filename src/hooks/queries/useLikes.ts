a1e9a187ad1a4d1c5529d68c50fc540c62089de3
하주영 <ju980304@naver.com>
2024-08-26 02:33:01 +0900

    Refactor: 좋아요 등록, 취소를 낙관적 업데이트로 변경
    - 변경 전 navgiate(-1) 때문에 좋아요 등록, 취소 하자마자 상태 변경을 위해 GET 요청이 필수 였음
    - 변경 후 낙관적 업데이트로 변경

--- src/hooks/queries/useLikes.ts
+++ src/hooks/queries/useLikes.ts
@@ -3,12 +3,11 @@ import { queryClient } from "@/api/queryClient";
 import { IResponseMessage } from "@/models/responseMessage.model";
 import { useMutation } from "@tanstack/react-query";
 
-export const useLikePost = (id: number) => {
+export const useLikePost = () => {
   const { mutate, isError } = useMutation<IResponseMessage, Error, number>({
     mutationFn: (id: number) => likePost(id),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["getHomeData"] });
-      queryClient.invalidateQueries({ queryKey: ["getCoachDetail", id] });
     }
   });
 
@@ -18,12 +17,11 @@ export const useLikePost = (id: number) => {
   };
 };
 
-export const useUnLikePost = (id: number) => {
+export const useUnLikePost = () => {
   const { mutate, isError } = useMutation<IResponseMessage, Error, number>({
     mutationFn: (id: number) => unlikePost(id),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["getHomeData"] });
-      queryClient.invalidateQueries({ queryKey: ["getCoachDetail", id] });
     }
   });
 
