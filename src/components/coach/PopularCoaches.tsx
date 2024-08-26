81c142c75ede54f3bee05bc4324ea6bc315ef4a2
하주영 <ju980304@naver.com>
2024-08-26 15:02:28 +0900

    Fix: 인기있는 코치에서 하트 클릭시 이동되는 문제 해결
    - Link와 하트 이벤트가 충돌

--- src/components/coach/PopularCoaches.tsx
+++ src/components/coach/PopularCoaches.tsx
@@ -1,7 +1,7 @@
 import profilePath from "@/assets/images/profile.png";
 import { IPopularCoach } from "@/models/coach.model";
 import { LineClamp } from "@/style/global";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -58,8 +58,16 @@ const CoachesSlider = ({ popularCoaches }: CoachesSliderProps) => {
 };
 
 const Coach = ({ coach }: CoachProps) => {
+  const navigate = useNavigate();
+  const handleLocation = (coachId: number) => {
+    navigate(`/coach/${coachId}`);
+  };
   return (
-    <CoachStyle to={`/coach/${coach.coachId}`}>
+    <CoachStyle
+      onClick={() => {
+        handleLocation(coach.coachId);
+      }}
+    >
       <img src={coach.profileImageUrl || profilePath} alt={coach.coachName} />
       <Heart checked={coach.isLiked} size="24px" id={coach.coachId} />
       <BoxText>
@@ -89,7 +97,7 @@ const RemovePadding = styled.div`
   margin: 0 -20px;
 `;
 
-const CoachStyle = styled(Link)`
+const CoachStyle = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: end;
