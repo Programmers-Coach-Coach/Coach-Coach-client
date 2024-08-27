27ef41ea5276907f872b5e49b4f986b057ad1bdd
하주영 <ju980304@naver.com>
2024-08-27 20:18:53 +0900

    Feat: 좋아요 오류일 때 낙관적 업데이터 x

--- src/hooks/queries/useLikes.ts
+++ src/hooks/queries/useLikes.ts
@@ -5,17 +5,21 @@ import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 export const useLikePost = (coachId: number) => {
-  const { mutate, isError } = useMutation<IResponseMessage, Error, number>({
+  const { mutate, isError, isSuccess } = useMutation<
+    IResponseMessage,
+    Error,
+    number
+  >({
     mutationFn: (id: number) => likePost(id),
     onSuccess: () => {
       toast.success("My 목록에 추가되었습니다!");
-      queryClient.invalidateQueries({ queryKey: ["getHomeData"] });
       queryClient.invalidateQueries({ queryKey: ["getCoachDetail", coachId] });
     }
   });
 
   return {
     mutate,
+    isSuccess,
     isError
   };
 };
