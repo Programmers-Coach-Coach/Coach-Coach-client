f158359bb09ed08ae679f29d451f0e1b57202f71
하주영 <ju980304@naver.com>
2024-08-28 00:11:22 +0900

    Feat: 리뷰 삭제, 생성 훅 생성

--- src/hooks/queries/useReview.ts
+++ src/hooks/queries/useReview.ts
@@ -1,5 +1,5 @@
 import { queryClient } from "@/api/queryClient";
-import { postReview } from "@/api/review.api";
+import { deleteReview, editReview, postReview } from "@/api/review.api";
 import { IResponseMessage } from "@/models/responseMessage.model";
 import { IPostReview } from "@/models/review.model";
 import { useMutation } from "@tanstack/react-query";
@@ -7,6 +7,11 @@ import toast from "react-hot-toast";
 
 interface IPostReviewVariables {
   coachId: number;
+  data: IPostReview;
+}
+
+interface IEditReviewVariables {
+  reviewId: number;
   data: IPostReview;
 }
 
@@ -24,6 +29,37 @@ export const usePostReview = (coachId: number) => {
     onError: () => {
       toast.error("리뷰는 한번만 남길 수 있습니다");
     }
+  });
+
+  return {
+    isError,
+    mutate
+  };
+};
+
+export const useEditReview = () => {
+  const { mutate, isError } = useMutation<
+    IResponseMessage,
+    Error,
+    IEditReviewVariables
+  >({
+    mutationFn: ({ reviewId, data }: IEditReviewVariables) =>
+      editReview(reviewId, data),
+    onSuccess: () => {},
+    onError: () => {}
+  });
+
+  return {
+    isError,
+    mutate
+  };
+};
+
+export const useDeleteReview = () => {
+  const { mutate, isError } = useMutation<IResponseMessage, Error, number>({
+    mutationFn: deleteReview,
+    onSuccess: () => {},
+    onError: () => {}
   });
 
   return {
