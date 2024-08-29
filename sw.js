22af7d23cd7d0e7aae480442bc0824bb98389e8f
하주영 <ju980304@naver.com>
2024-08-29 23:48:33 +0900

    Feat: pwa 설정

---
+++ sw.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] installed");
+});
+
+// activate event
+self.addEventListener("activate", (e) => {
+  console.log("[Service Worker] actived", e);
+});
+
+// fetch event
+self.addEventListener("fetch", (e) => {
+  console.log("[Service Worker] fetched resource " + e.request.url);
+});
