55234fcbc20fe6fcb8a7d4eec6671db0caafb293
yoonsaeng <truestar1997@gmail.com>
2024-10-22 18:33:36 +0900

    Modify: 데이터 상태 & 타입 변경

--- src/data/routine.ts
+++ src/data/routine.ts
@@ -1,28 +1,25 @@
-import { IGetRoutine, IRoutineDetails } from "@/models/routine.model";
+import { IGetRoutineList } from "@/models/routine.model";
 import * as faker from "@/utils/faker";
+import { sportList } from "./sportsList";
 
-export const PER_PAGE = 10;
-
-export const routines: IGetRoutine[] = Array.from({ length: 1 }).map(
-  (_, i) => ({
+export const getRoutines: IGetRoutineList = {
+  completionPercentage: 0,
+  routines: Array.from({ length: 5 }).map((_, i) => ({
     routineId: i + 1,
     routineName: faker.fullname(),
-    sportName: faker.fullname()
-  })
-);
-
-export const routineDetail: Omit<IRoutineDetails, "routineName"> = {
-  categoryList: Array.from({ length: 3 }).map((_, i) => ({
-    categoryId: i + 1,
-    categoryName: faker.fullname(),
-    isCompleted: false,
-    actionList: Array.from({ length: 4 }).map((_, j) => ({
+    sportName: sportList[Math.floor(Math.random() * 12)].sportName,
+    isCompleted: Math.random() < 0.5,
+    actions: Array.from({ length: 3 }).map((_, j) => ({
       actionId: i * 100 + j * 100 + 1,
       actionName: faker.fullname(),
       sets: 3,
-      count: Math.floor(Math.random() * 12),
-      minutes: Math.floor(Math.random() * 12),
-      description: faker.fullname()
+      countsOrMinutes: Math.floor(Math.random() * 60)
     }))
   }))
 };
+
+getRoutines.completionPercentage = Math.floor(
+  (getRoutines.routines.filter((routine) => routine.isCompleted).length /
+    getRoutines.routines.length) *
+    100
+);
---
+++ src/hooks/queries/useRoutine.ts
@@ -0,0 +1,111 @@
+import { queryClient } from "@/api/queryClient";
+import {
+  deleteRoutineData,
+  getRoutinesData,
+  patchRoutineData,
+  postRoutineData
+} from "@/api/routine.api";
+import { IResponseMessage } from "@/models/responseMessage.model";
+import {
+  IRoutineQuery,
+  IGetRoutineList,
+  IPostPatchRoutine
+} from "@/models/routine.model";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export const useGetRoutines = ({ coachId, userId }: IRoutineQuery = {}) => {
+  const { data, isLoading, isError } = useQuery<IGetRoutineList>({
+    queryKey: ["getRoutinesData", coachId, userId],
+    queryFn: () => getRoutinesData({ coachId, userId })
+  });
+
+  return {
+    data,
+    isLoading,
+    isError
+  };
+};
+
+export const usePostRoutine = () => {
+  const { mutate, isPending, isError, data } = useMutation<
+    IResponseMessage,
+    Error,
+    IPostPatchRoutine
+  >({
+    mutationFn: postRoutineData,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["getRoutinesData"] });
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        toast.error(
+          error.response?.data?.message || "요청 중 오류가 발생했습니다."
+        );
+      }
+    }
+  });
+
+  return {
+    mutate, // POST 요청을 수행하는 함수
+    isLoading: isPending,
+    isError,
+    data // 성공 시 반환된 데이터
+  };
+};
+
+export const usePatchRoutine = () => {
+  const { mutate, isPending, isError, data } = useMutation<
+    IResponseMessage,
+    Error,
+    { payload: IPostPatchRoutine; routineId: number }
+  >({
+    mutationFn: ({ payload, routineId }) =>
+      patchRoutineData(payload, routineId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["getRoutinesData"] });
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        toast.error(
+          error.response?.data?.message || "요청 중 오류가 발생했습니다."
+        );
+      }
+    }
+  });
+
+  return {
+    mutate,
+    isLoading: isPending,
+    isError,
+    data
+  };
+};
+
+export const useDeleteRoutine = () => {
+  const { mutate, isPending, isError, data } = useMutation<
+    IResponseMessage,
+    Error,
+    number
+  >({
+    mutationFn: deleteRoutineData,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["getRoutinesData"] });
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        toast.error(
+          error.response?.data?.message || "요청 중 오류가 발생했습니다."
+        );
+      }
+    }
+  });
+
+  return {
+    mutate,
+    isLoading: isPending,
+    isError,
+    data
+  };
+};
