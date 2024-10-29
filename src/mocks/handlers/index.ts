af46f6ece4f660da2f40be2e475fff4379fa3a79
yoonsaeng <truestar1997@gmail.com>
2024-10-29 14:25:08 +0900

    Delete: Member Mock API 삭제

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,9 +1,4 @@
 import { handlers as coachHandlers } from "./coach";
 import { handlers as routineHandlers } from "./routine";
-import { handlers as memberHandler } from "./member";
 
-export const handlers = [
-  ...coachHandlers,
-  ...routineHandlers,
-  ...memberHandler
-];
+export const handlers = [...coachHandlers, ...routineHandlers];
