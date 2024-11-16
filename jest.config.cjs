eca0148ac513e7ccc71fb019eb90c67f7e8d1b44
yoonsaeng <truestar1997@gmail.com>
2024-11-16 13:53:30 +0900

    Fix: svg jest 에러 처리

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
 // package.json에서 type이 module 이므로 파일 확장명이 commonJs인 cjs로 사용
 module.exports = {
-  resolver: "jest-resolve",
   testEnvironment: "jest-environment-jsdom",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testMatch: [
@@ -37,7 +36,8 @@ module.exports = {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^/public/(.*)$": "<rootDir>/public/$1", // public 디렉토리의 모든 파일 매핑
     "^.+\\.(png|jpg|jpeg|gif|svg)$": "jest-transform-stub", // 기타 이미지 파일 매핑
-    "^@/(.*)$": "<rootDir>/src/$1" // tsconfig에서 설정한 alias를 Jest에서 매핑
+    "^@/(.*)$": "<rootDir>/src/$1", // tsconfig에서 설정한 alias를 Jest에서 매핑
+    "\\.(svg)$": "<rootDir>/__mocks__/fileMock.js" // SVG 파일 처리
   },
   setupFiles: ["<rootDir>/test-setup.js"]
 };
