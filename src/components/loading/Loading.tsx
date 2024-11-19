0633833d67a41f9882cf9f70080fcbc5e013b000
하주영 <ju980304@naver.com>
2024-11-19 20:23:04 +0900

    Feat: 로딩 사이즈 축소

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -8,7 +8,7 @@ const Loading = ({ textDisabled }: Props) => {
   return (
     <Container>
       {!textDisabled && <h3>잠시만 기다려주세요.</h3>}
-      <FadeLoader color="#5271FF" />
+      <FadeLoader color="#5271FF" width="5px" height="5px" />
     </Container>
   );
 };
@@ -20,6 +20,13 @@ const Container = styled.div`
   align-items: center;
   gap: 20px;
   height: 70vh;
+
+  h3 {
+    font-size: 15px;
+    font-weight: 500;
+    line-height: 23px;
+    letter-spacing: -0.375px;
+  }
 `;
 
 export default Loading;
